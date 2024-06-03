6eb796edc20bd4abd321183592f68bafc9a69ce2
wolfdev0221 <george.shopify97@gmail.com>
2024-06-03 19:34:11 -0400

    updates

--- index.js
+++ index.js
@@ -2,320 +2,41 @@ import express from "express";
 import crypto from "crypto";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { config } from "dotenv";
-import { createProductsOnStore, updateProductOnStore } from "./Services/vgc.js";
-
-config();
 
 const app = express();
 const port = 3001;
 
 const SECRET_KEY = "a0e9b07e12f144dea93f44d859fe6916";
 
-const stateMapping = {
-  Alabama: "AL",
-  Alaska: "AK",
-  Arizona: "AZ",
-  Arkansas: "AR",
-  California: "CA",
-  Colorado: "CO",
-  Connecticut: "CT",
-  Delaware: "DE",
-  Florida: "FL",
-  Georgia: "GA",
-  Hawaii: "HI",
-  Idaho: "ID",
-  Illinois: "IL",
-  Indiana: "IN",
-  Iowa: "IA",
-  Kansas: "KS",
-  Kentucky: "KY",
-  Louisiana: "LA",
-  Maine: "ME",
-  Maryland: "MD",
-  Massachusetts: "MA",
-  Michigan: "MI",
-  Minnesota: "MN",
-  Mississippi: "MS",
-  Missouri: "MO",
-  Montana: "MT",
-  Nebraska: "NE",
-  Nevada: "NV",
-  "New Hampshire": "NH",
-  "New Jersey": "NJ",
-  "New Mexico": "NM",
-  "New York": "NY",
-  "North Carolina": "NC",
-  "North Dakota": "ND",
-  Ohio: "OH",
-  Oklahoma: "OK",
-  Oregon: "OR",
-  Pennsylvania: "PA",
-  "Rhode Island": "RI",
-  "South Carolina": "SC",
-  "South Dakota": "SD",
-  Tennessee: "TN",
-  Texas: "TX",
-  Utah: "UT",
-  Vermont: "VT",
-  Virginia: "VA",
-  Washington: "WA",
-  "West Virginia": "WV",
-  Wisconsin: "WI",
-  Wyoming: "WY",
-};
-
-const countryMapping = {
-  Afghanistan: "AF",
-  Albania: "AL",
-  Algeria: "DZ",
-  Andorra: "AD",
-  Angola: "AO",
-  "Antigua and Barbuda": "AG",
-  Argentina: "AR",
-  Armenia: "AM",
-  Australia: "AU",
-  Austria: "AT",
-  Azerbaijan: "AZ",
-  Bahamas: "BS",
-  Bahrain: "BH",
-  Bangladesh: "BD",
-  Barbados: "BB",
-  Belarus: "BY",
-  Belgium: "BE",
-  Belize: "BZ",
-  Benin: "BJ",
-  Bhutan: "BT",
-  Bolivia: "BO",
-  "Bosnia and Herzegovina": "BA",
-  Botswana: "BW",
-  Brazil: "BR",
-  Brunei: "BN",
-  Bulgaria: "BG",
-  "Burkina Faso": "BF",
-  Burundi: "BI",
-  "Cabo Verde": "CV",
-  Cambodia: "KH",
-  Cameroon: "CM",
-  Canada: "CA",
-  "Central African Republic": "CF",
-  Chad: "TD",
-  Chile: "CL",
-  China: "CN",
-  Colombia: "CO",
-  Comoros: "KM",
-  "Congo, Democratic Republic of the": "CD",
-  "Congo, Republic of the": "CG",
-  "Costa Rica": "CR",
-  Croatia: "HR",
-  Cuba: "CU",
-  Cyprus: "CY",
-  "Czech Republic": "CZ",
-  Denmark: "DK",
-  Djibouti: "DJ",
-  Dominica: "DM",
-  "Dominican Republic": "DO",
-  Ecuador: "EC",
-  Egypt: "EG",
-  "El Salvador": "SV",
-  "Equatorial Guinea": "GQ",
-  Eritrea: "ER",
-  Estonia: "EE",
-  Eswatini: "SZ",
-  Ethiopia: "ET",
-  Fiji: "FJ",
-  Finland: "FI",
-  France: "FR",
-  Gabon: "GA",
-  Gambia: "GM",
-  Georgia: "GE",
-  Germany: "DE",
-  Ghana: "GH",
-  Greece: "GR",
-  Grenada: "GD",
-  Guatemala: "GT",
-  Guinea: "GN",
-  "Guinea-Bissau": "GW",
-  Guyana: "GY",
-  Haiti: "HT",
-  Honduras: "HN",
-  Hungary: "HU",
-  Iceland: "IS",
-  India: "IN",
-  Indonesia: "ID",
-  Iran: "IR",
-  Iraq: "IQ",
-  Ireland: "IE",
-  Israel: "IL",
-  Italy: "IT",
-  Jamaica: "JM",
-  Japan: "JP",
-  Jordan: "JO",
-  Kazakhstan: "KZ",
-  Kenya: "KE",
-  Kiribati: "KI",
-  "Korea, North": "KP",
-  "Korea, South": "KR",
-  Kosovo: "XK",
-  Kuwait: "KW",
-  Kyrgyzstan: "KG",
-  Laos: "LA",
-  Latvia: "LV",
-  Lebanon: "LB",
-  Lesotho: "LS",
-  Liberia: "LR",
-  Libya: "LY",
-  Liechtenstein: "LI",
-  Lithuania: "LT",
-  Luxembourg: "LU",
-  Madagascar: "MG",
-  Malawi: "MW",
-  Malaysia: "MY",
-  Maldives: "MV",
-  Mali: "ML",
-  Malta: "MT",
-  "Marshall Islands": "MH",
-  Mauritania: "MR",
-  Mauritius: "MU",
-  Mexico: "MX",
-  Micronesia: "FM",
-  Moldova: "MD",
-  Monaco: "MC",
-  Mongolia: "MN",
-  Montenegro: "ME",
-  Morocco: "MA",
-  Mozambique: "MZ",
-  Myanmar: "MM",
-  Namibia: "NA",
-  Nauru: "NR",
-  Nepal: "NP",
-  Netherlands: "NL",
-  "New Zealand": "NZ",
-  Nicaragua: "NI",
-  Niger: "NE",
-  Nigeria: "NG",
-  "North Macedonia": "MK",
-  Norway: "NO",
-  Oman: "OM",
-  Pakistan: "PK",
-  Palau: "PW",
-  Panama: "PA",
-  "Papua New Guinea": "PG",
-  Paraguay: "PY",
-  Peru: "PE",
-  Philippines: "PH",
-  Poland: "PL",
-  Portugal: "PT",
-  Qatar: "QA",
-  Romania: "RO",
-  Russia: "RU",
-  Rwanda: "RW",
-  "Saint Kitts and Nevis": "KN",
-  "Saint Lucia": "LC",
-  "Saint Vincent and the Grenadines": "VC",
-  Samoa: "WS",
-  "San Marino": "SM",
-  "Sao Tome and Principe": "ST",
-  "Saudi Arabia": "SA",
-  Senegal: "SN",
-  Serbia: "RS",
-  Seychelles: "SC",
-  "Sierra Leone": "SL",
-  Singapore: "SG",
-  Slovakia: "SK",
-  Slovenia: "SI",
-  "Solomon Islands": "SB",
-  Somalia: "SO",
-  "South Africa": "ZA",
-  Spain: "ES",
-  "Sri Lanka": "LK",
-  Sudan: "SD",
-  "Sudan, South": "SS",
-  Suriname: "SR",
-  Sweden: "SE",
-  Switzerland: "CH",
-  Syria: "SY",
-  Taiwan: "TW",
-  Tajikistan: "TJ",
-  Tanzania: "TZ",
-  Thailand: "TH",
-  "Timor-Leste": "TL",
-  Togo: "TG",
-  Tonga: "TO",
-  "Trinidad and Tobago": "TT",
-  Tunisia: "TN",
-  Turkey: "TR",
-  Turkmenistan: "TM",
-  Tuvalu: "TV",
-  Uganda: "UG",
-  Ukraine: "UA",
-  "United Arab Emirates": "AE",
-  "United Kingdom": "GB",
-  "United States": "US",
-  Uruguay: "UY",
-  Uzbekistan: "UZ",
-  Vanuatu: "VU",
-  "Vatican City": "VA",
-  Venezuela: "VE",
-  Vietnam: "VN",
-  Yemen: "YE",
-  Zambia: "ZM",
-  Zimbabwe: "ZW",
-};
-
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/import-shopify-products", async (req, res) => {
-  if (req.body.product_urls.length > 0) {
-    const importedProductCounts = await createProductsOnStore(
-      req.body.product_urls
-    );
-    return res.status(200).json({
-      "number of imported products": importedProductCounts,
-      "number of required products": req.body.product_urls.length,
-    });
-  }
-  return res.status(422).json({ error: "No urls provided" });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.post("/rye-webhooks", async (req, res) => {
+app.post("/", (req, res) => {
   console.log("rye webhook received");
+  console.log(req.body);
+  // Create a SHA-256 HMAC with the shared secret key
+  const hmac = crypto.createHmac("sha256", SECRET_KEY);
 
-  switch (req.body.type) {
-    case "PRODUCT_UPDATED":
-      const result = await updateProductOnStore(req.body.data.product);
-      if (result) return res.status(200).send("Product updated");
-      break;
-    default:
-      break;
+  // Update the HMAC with the request body
+  // req.body represents the POST body as a string, assuming that it hasn't been parsed to JSON
+  // hmac.update(req.body);
+  return res.status(200);
+
+  // Compare the base64 HMAC digest against the signature passed in the header
+  if (hmac.digest("base64") !== req.headers["rye-hmac-signature-v1"]) {
+    // The request is not authentic
+    return res.status(401).send("Unauthorized");
   }
 });
 
-// app.post("/", (req, res) => {
-//   console.log("rye webhook received");
-//   console.log(req.body);
-//   // Create a SHA-256 HMAC with the shared secret key
-//   const hmac = crypto.createHmac("sha256", SECRET_KEY);
-
-//   // Update the HMAC with the request body
-//   // req.body represents the POST body as a string, assuming that it hasn't been parsed to JSON
-//   // hmac.update(req.body);
-//   return res.status(200);
-
-//   // Compare the base64 HMAC digest against the signature passed in the header
-//   if (hmac.digest("base64") !== req.headers["rye-hmac-signature-v1"]) {
-//     // The request is not authentic
-//     return res.status(401).send("Unauthorized");
-//   }
-// });
-
-app.post("/vgc-order-paid", (req, res) => {
-  const order_info = {
-    id: req.body.id,
-    products: req.body.line_items,
-    billingInfo: req.body.billing_address,
-  };
+app.post("/test", (req, res) => {
+  console.log("webhook received");
+  console.log(req.body);
+  return res.send("success");
 });
 
 app.post("/vgc-shipping", (req, res) => {
@@ -328,6 +49,7 @@ app.post("/vgc-shipping", (req, res) => {
     countryCode: countryMapping[req.body.country] || null,
     postalCode: req.body.zip,
   };
+  console.log(shipping_info);
 });
 
 app.listen(port, () => {
